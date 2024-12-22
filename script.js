2f0970bc4d6e9549ca865aa6b6ff0dcea5c8e200
Ajetunmobi Anjolaoluwa <abdfashion.2014@gmail.com>
2024-12-22 19:45:59 +0100

    calculator

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const display = document.getElementById("display");
+const display =document.getElementById("display");
 const buttons = document.querySelectorAll(".btn,.btn-operator,.btn-equal");
+let input ="";
 buttons.forEach(button=>{
     button.addEventListener("click",()=>{
-        const value =button.innerText;
-        if(value=== "C"){
-            display.innerText="";
-        }else if (value === "-"){
-            try(
-                display.innerText =eval(display.innerText.)
-            )
+        const value = button.textContent;
+        if (value==="C"){
+            input="";
+            display.textContent="";
         }
-})
-})
+        else if(value === "="){
+            input =input.replace(/×/g,"*").replace(/÷/g,"/");
+            display.textContent=eval(input);
+            input =display.textContent;
+        }
+        else{
+            input+=value;
+            display.textContent=input;
+        }
+    });
+});
